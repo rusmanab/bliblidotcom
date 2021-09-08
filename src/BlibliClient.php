018fe261bae2192f0cba3bb26472a10636084ce3
rusmanab <rusmanab@gmail.com>
2021-09-08 18:59:59 +0700

    update

--- src/BlibliClient.php
+++ src/BlibliClient.php
@@ -7,7 +7,6 @@ use Rusmanab\Blibli\Module\Product;
 
 class BlibliClient{
 
-    protected $baseUrl = "https://api.blibli.com/v2/";
     protected $module = [];
     protected $mtaApi = ""; 
     protected $mtaPwd = ""; 
@@ -17,7 +16,7 @@ class BlibliClient{
     protected $partnerUname= "";
     protected $partnerKey= "";
     
-    
+
     public function __construct()
     {        
         $this->module['product']  = new Product($this);
@@ -68,12 +67,7 @@ class BlibliClient{
             'Api-Seller-Key: '. $this->partnerKey,
         );
         
-        $params = array(
-            'requestId' => 'bar',   
-            'businessPartnerCode' => $this->partnerCode,
-            'username' => $this->partnerUname,
-            'channelId' => $this->channelId,
-        );
+        $params = array('foo' => 'bar');
         $uri = $uri . '?' . http_build_query($params);
 
         $jsonBody = json_encode($data);
